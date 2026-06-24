34fcfb84af5495e39044c3768f49a618a6405b7f
hkw492 <hemanthbmw.10@gmail.com>
2026-06-24 14:08:12 +0530

    Add UIManager with balance subscription

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public Text balanceText;     
+    public Text messageText;      
+    public Text payoutTableText;  
+    public Button spinButton;
+
+    void OnEnable()
+    {
+        BalanceManager.OnBalanceChanged += UpdateBalanceDisplay;
+    }
+
+    void OnDisable()
+    {
+        BalanceManager.OnBalanceChanged -= UpdateBalanceDisplay;
+    }
+
+    void UpdateBalanceDisplay(int balance)
+    {
+        balanceText.text = "Balance: Rs " + balance;
+    }
+
+    public void ShowWin(int amount)
+    {
+        messageText.text = "WIN!  +" + amount;
+        messageText.color = Color.yellow;
+    }
+
+    public void ShowLose()
+    {
+        messageText.text = "Try Again";
+        messageText.color = Color.white;
+    }
+
+    public void ShowMessage(string msg)
+    {
+        messageText.text = msg;
+        messageText.color = Color.red;
+    }
+
+    public void SetSpinButtonInteractable(bool state)
+    {
+        spinButton.interactable = state;
+    }
+}
